310ff5881906c1c93c180373c47b0eb41e2c9913
CJAllen056 <christopherjallen056@gmail.com>
2016-04-22 13:11:44 +0100

    devise setup done for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable :recoverable,
+	devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 end
--- db/migrate/20160422103659_create_users.rb
+++ db/migrate/20160422103659_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :email
       t.string :image
       t.string :fname
       t.string :lname
