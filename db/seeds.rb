62c3874ff6c284c4d5133dfbc40332b4933d3add
CJAllen056 <christopherjallen056@gmail.com>
2016-04-22 17:12:32 +0100

    genre seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Group.destroy_all
+Book.destroy_all
+Genre.destroy_all
+
+[
+	{ name: "Art, Architecture and Photography" },
+	{ name: "Biography" },
+	{ name: "Business, Finance and Law" },
+	{ name: "Children's Books" },
+	{ name: "Comics and Graphic Novels" },
+	{ name: "Computing and Internet" },
+	{ name: "Crime, Thrillers and Mystery" },
+	{ name: "Education Studies and Teaching" },
+	{ name: "Fiction" },
+	{ name: "Food and Drink" },
+	{ name: "Gay and Lesbian" },
+	{ name: "Health, Family and Lifestyle" },
+	{ name: "History" },
+	{ name: "Home and Garden" },
+	{ name: "Horror" },
+	{ name: "Humour" },
+	{ name: "Languages" },
+	{ name: "Mind, Body and Spirit" },
+	{ name: "Music, Stage and Screen" },
+	{ name: "Poetry, Drama and Criticism" },
+	{ name: "Religion and Spirituality" },
+	{ name: "Romance" },
+	{ name: "Science and Nature" },
+	{ name: "Science Fiction and Fantasy" },
+	{ name: "Society, Politics and Philosophy" },
+	{ name: "Sports, Hobbies and Games" },
+	{ name: "Travel and Holiday" },
+	{ name: "Young Adult" }
+].each do |genre|
+	Genre.create!(genre)
+end
