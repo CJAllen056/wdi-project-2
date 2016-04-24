43b3a1806d2f3c9ab98f5d937b836fb850fc5027
CJAllen056 <christopherjallen056@gmail.com>
2016-04-24 19:26:15 +0100

    edit and new forms for books working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,15 +10,31 @@ class BooksController < ApplicationController
    end
 
    def new
+      @book = Book.new
    end
 
    def create
+      @book = Book.new(book_params)
+      if @book.save
+         flash[:success] = "The book has been added"
+         redirect_to books_path
+      else
+         render "new"
+      end 
    end
 
    def edit
+      @book = Book.find(params[:id])
    end
 
    def update
+      @book = Book.find(params[:id])
+      if @book.update(book_params)
+         flash[:success] = "Changes have been made successfully"
+         redirect_to book_path
+      else
+         render "edit"
+      end
    end
 
    def delete
