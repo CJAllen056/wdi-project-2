016368f751711f0c73dba64638bd929e7d84c209
CJAllen056 <christopherjallen056@gmail.com>
2016-05-20 12:26:19 +0100

    working on deployment

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module WdiProject2
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_file = true
   end
 end
